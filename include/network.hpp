ea575d3a969a52d9ac884eb7bf6ff89627f4fe4f
SJ <dsergiojonathan@gmail.com>
2022-04-23 20:53:35 -0500

    added mutation option and vectorxf input for forward

--- include/network.hpp
+++ include/network.hpp
@@ -6,6 +6,10 @@
 
 using namespace Eigen;
 
+/**
+ * @brief Fully-Connected Neural Network
+ * 
+ */
 class Network {
     
     private:
@@ -15,11 +19,50 @@ class Network {
     
     public:
     
+    /**
+     * @brief Construct a new Network object
+     * 
+     * @param dims Specify the dimensions of each layer.
+     * 
+     * The length represents the number of layers, with
+     * the first representing the input and the last representing
+     * the output. 
+     */
     Network(std::vector<int>& dims);
+    
+    /**
+     * @brief Construct a copy of base with mutation
+     * 
+     * @param base Network to mutate
+     * @param mutation_rate Determines magnitude of mutation, 
+     * similar to learning rate. Use values between 0 and 1.
+     */
+    Network(Network& base, float mutation_rate);
+    
+    // ACTIVATION FUNCTIONS
+
     float relu(float x);
-    float sigmoid(float x);
     MatrixXf relu(MatrixXf& X);
+    
+    float sigmoid(float x);
     MatrixXf sigmoid(MatrixXf& X);
+    
+    // OPERATIONS
+    
+    /**
+     * @brief Perform forward propagation
+     * 
+     * @param X Shape: (NUM_FEATURES)
+     * @return Shape: (NUM_OUTPUTS)
+     */
+    VectorXf forward(VectorXf& X);
+        
+    /**
+     * @brief Perform forward propagation
+     * 
+     * @param X Shape: (NUM_EXAMPLES, NUM_FEATURES)
+     * @return Shape: (NUM_EXAMPLES, NUM_OUTPUTS)
+     */
     MatrixXf forward(MatrixXf& X);
 };
 
--- src/network.cpp
+++ src/network.cpp
@@ -1,20 +1,39 @@
 #include <cmath>
 #include "network.hpp"
+#include <iostream>
+
+// CONSTRUCTORS
 
 Network::Network(std::vector<int>& dims) {
     for (int i = 0; i < dims.size()-1; i++) {
-        W.push_back(MatrixXf::Random(dims[i+1],dims[i]));
-        b.push_back(VectorXf::Random(dims[i+1]));
+        // he initialization
+        const float HE = (1.0f / sqrtf(dims[i+1]/2.0f));
+        W.push_back(MatrixXf::Random(dims[i+1],dims[i]) * HE);
+        b.push_back(VectorXf::Zero(dims[i+1]));
     }
 }
 
+Network::Network(Network& base, float mutation_rate) {
+    for (int i = 0; i < base.W.size(); i++) {
+        // grab the base values
+        MatrixXf W_i = base.W[i].eval();
+        MatrixXf b_i = base.b[i].eval();
+        
+        // find the mutation weights
+        const float HE =  (mutation_rate / sqrtf(W_i.rows()/2.0f));
+        MatrixXf randW_i = MatrixXf::Random(W_i.rows(), W_i.cols()) * HE;
+        MatrixXf randb_i = VectorXf::Random(b_i.rows()) * HE;
+        
+        //
+        W.push_back(W_i+randW_i);
+        b.push_back(b_i+randb_i);
+    }
+}
+
+// ACTIVATION FUNCTIONS
 
 float Network::relu(float x){
     return (x > 0) ? x : 0;
-}
-
-float Network::sigmoid(float x){
-    return 1 / (1 + exp(x));
 }
 
 MatrixXf Network::relu(MatrixXf& X) {
@@ -27,6 +46,10 @@ MatrixXf Network::relu(MatrixXf& X) {
     return A;
 }
 
+float Network::sigmoid(float x){
+    return 1 / (1 + expf(-x));
+}
+
 MatrixXf Network::sigmoid(MatrixXf& X) {
     MatrixXf A(X.rows(), X.cols());
     for (int r = 0; r < X.rows(); r++) {
@@ -37,21 +60,41 @@ MatrixXf Network::sigmoid(MatrixXf& X) {
     return A;
 }
 
-MatrixXf Network::forward(MatrixXf& X) {
-    MatrixXf A = X.transpose();
-    const int L = W.size();
+// OPERATIONS
+
+VectorXf Network::forward(VectorXf& X) {
+    const int L = W.size(); // num layers
+    
+    VectorXf A = X.eval();
     
     for (int l = 0; l < L-1; l++) {
         MatrixXf Z = W[l] * A;
         Z.colwise() += b[l];
-        A = relu(Z);
+        A = relu(Z).eval();
     }
     
     MatrixXf Z = W[L-1] * A;
     Z.colwise() += b[L-1];
+    A = sigmoid(Z).eval();
     
-    A = sigmoid(Z);
-    return A.transpose();
+    return A;
 }
 
-
+MatrixXf Network::forward(MatrixXf& X) {
+    const int L = W.size(); // num layers
+    
+    MatrixXf A = X.transpose().eval();
+    
+    for (int l = 0; l < L-1; l++) {
+        MatrixXf Z = W[l] * A;
+        Z.colwise() += b[l];
+        A = relu(Z).eval();
+    }
+    
+    MatrixXf Z = W[L-1] * A;
+    Z.colwise() += b[L-1];
+    A = sigmoid(Z).eval();
+    
+    A.transposeInPlace();
+    return A;
+}
